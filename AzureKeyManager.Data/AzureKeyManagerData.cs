be093a953e305725831723263e2742faad059523
surajaher98 <45867140+surajaher98@users.noreply.github.com>
2020-06-23 15:32:42 +0530

    added unnecessary variables

--- AzureKeyManager.Data/AzureKeyManagerData.cs
+++ AzureKeyManager.Data/AzureKeyManagerData.cs
@@ -58,6 +58,12 @@ namespace AzureKeyManager.Data
 
     public bool submitKey(core.AzureKeyManager azureNet)
     {
+      var abc = "";
+      var pqr = "";
+      var lmn = "";
+      var xyz = "";
+      var def = "";
+      var ghi = "";
       publicazure = azureNet; 
       KeyVaultClient kv = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(GetToken));
       foreach (var item in azureNet.submitSecrets)
